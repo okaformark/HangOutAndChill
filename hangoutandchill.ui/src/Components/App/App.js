a8acf6c983a05fb439eeeda39c7a7cfebee7aeb0
okaformark <okaformark@gmail.com>
2020-01-21 12:33:08 -0600

    react setup complete

---
+++ hangoutandchill.ui/src/Components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button className="button btn btn-danger">X</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
